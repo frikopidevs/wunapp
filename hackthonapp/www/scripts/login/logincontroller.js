1b489d9f8449d07defbe8295993166c17e190e70
frikopi <frikopidevs@gmail.com>
2016-11-20 07:28:16 +0800

    renamed app

--- hackthonapp/www/scripts/login/logincontroller.js
+++ hackthonapp/www/scripts/login/logincontroller.js
@@ -2,15 +2,45 @@
 	'use strict';
 
 angular.module('login.module')
-	   .controller('loginCtrl',function($scope,$http,$state,userService){
+	   .controller('loginCtrl',function($scope,$http,$state,userService,$cordovaGeolocation,$ionicPopup){
       
 	    $scope.data ={
         'username':'',
         'password':''
       }
+      var posOptions = {timeout: 10000, enableHighAccuracy: false};
+
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(function(position){
+      $scope.lat  = position.coords.latitude
+          $scope.long = position.coords.longitude
+      }, function(error){
+        console.log("Could not get location");
+    });
+
+
+     $scope.showAlertInvalid = function() {
+       var alertPopup = $ionicPopup.alert({
+         title: 'Need GPS',
+         template: 'Please Turn on the GPS and Restart the App'
+       });
+       alertPopup.then(function(res) {
+       });
+
+       var posOptions = {timeout: 10000, enableHighAccuracy: false};
+      
+      $cordovaGeolocation.getCurrentPosition(posOptions).then(function(position){
+      $scope.lat  = position.coords.latitude
+          $scope.long = position.coords.longitude
+      }, function(error){
+        console.log("Could not get location");
+    });
+
+     };
+
 
       $scope.login = function(){
         //$state.go('home');
+        if($scope.lat){
          $http.get("http://pldthackathon.kimseanpusod.com/api/loginauth.php?username="+$scope.data.username+"&&password="+$scope.data.password)
                   .then(function successCallback(response) {
                     // console.log(response.data);
@@ -22,6 +52,7 @@ angular.module('login.module')
                     else if($scope.result.role == "user"){
                       userService.adduserdetails(response.data);
                        $state.go('home');
+                       $scope.data.password = '';
                     }
                     else if($scope.result.role == "agent"){
                       userService.adduserdetails(response.data);
@@ -33,6 +64,9 @@ angular.module('login.module')
                   },function errorCallback(response){
                     console.log("Error");
                   });
+        }else{
+          $scope.showAlertInvalid();
+        }
 
       }
 
